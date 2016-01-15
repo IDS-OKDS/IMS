73a038dd6520f6562cf76fd352703252968b8786
Joe McPherson <j.m.p.mcpherson@gmail.com>
2016-01-15 16:51:09 +0000

    add description highlight functionality to bookmarklet

--- themes/ids_ims_adminimal/js/ids_ims_adminimal.js
+++ themes/ids_ims_adminimal/js/ids_ims_adminimal.js
@@ -1,48 +1,53 @@
 (function ($, Drupal, window, document, undefined) {
 
-Drupal.behaviors.ids_ims_admin = {
-  attach: function(context, settings) {
+    Drupal.behaviors.ids_ims_admin = {
+        attach: function (context, settings) {
 
-    $(window).ready(function() {
-      // Execute code once the window is ready.
-    });
+            $(window).ready(function () {
+                // Execute code once the window is ready.
+            });
 
-    $(window).load(function() {
-      // Execute code once the window is fully loaded.
-      var href = window.location.href;
-      var href_arr = href.split('#');
-      if(href_arr[1] != undefined){
-          var popupquery = href_arr[1];
-          var popupquery_arr = popupquery.split('&');
-          for( var query_item_raw in popupquery_arr) {    
-                var query_item = popupquery_arr[query_item_raw].split('=');
-                if(query_item[1] != undefined){
-                    var value_for_form = unescape(query_item[1]);
-                    if(query_item[0] == 'title'){
-                        $('#edit-title').val(value_for_form);
+            $(window).load(function () {
+
+            });
+
+            $(window).resize(function () {
+                // Execute code when the window is resized.
+            });
+
+            $(window).scroll(function () {
+                // Execute code when the window scrolls.
+            });
+
+            $(document).ready(function () {
+                // Execute code once the DOM is ready.
+                // Execute code once the window is fully loaded.
+                var href = window.location.href;
+                var href_arr = href.split('#');
+                if (href_arr[1] != undefined) {
+                    var popupquery = href_arr[1];
+                    var popupquery_arr = popupquery.split('&');
+                    for (var query_item_raw in popupquery_arr) {
+                        var query_item = popupquery_arr[query_item_raw].split('=');
+                        if (query_item[1] != undefined) {
+                            var value_for_form = unescape(query_item[1]);
+                            if (query_item[0] == 'title') {
+                                $('#edit-title').val(value_for_form);
+                            }
+                            if (query_item[0] == 'url') {
+                                $('#edit-field-external-urls-und-0-value').val(value_for_form);
+                            }
+                            if (query_item[0] == 'description') {
+                                $('#edit-body-und-0-value').val(value_for_form);
+                            }
+                        }
                     }
-                    if(query_item[0] == 'url'){
-                        $('#edit-field-external-urls-und-0-value').val(value_for_form);
-                    }
+                    //var highlighted_text = getOpenerSelectionText();
+                    //$('#edit-field-language-und-0-value').val(highlighted_text); 
                 }
-            }
-      }
-    });
-
-    $(window).resize(function() {
-      // Execute code when the window is resized.
-    });
-
-    $(window).scroll(function () {
-      // Execute code when the window scrolls.
-    });
-
-    $(document).ready(function() {
-      // Execute code once the DOM is ready.
-      
-    });
-  }
-};
+            });
+        }
+    };
 
 
 })(jQuery, Drupal, this, this.document);
