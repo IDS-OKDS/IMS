d409c46b26062cde46ff7c3cb11fb581c4065779
Joe McPherson <j.m.p.mcpherson@gmail.com>
2016-01-20 14:56:27 +0000

    append Asset ID to Title in node View/Edit

--- themes/ids_ims_adminimal/template.php
+++ themes/ids_ims_adminimal/template.php
@@ -5,3 +5,23 @@
  * This file contains the main theme functions hooks and overrides.
  */
 
+
+/**
+ * Implementation of hook_preprocess_page
+ */
+function ids_ims_adminimal_preprocess_page(&$vars) {
+	$node = (isset($vars['node'])) ? $vars['node'] : NULL;
+
+	$lang_code = ($node) ? entity_language('node', $node) : LANGUAGE_NONE;
+
+	$vars['lang_code'] = $lang_code;
+	
+	$vars['asset_id'] = '';
+
+	if (isset($vars['node']->type) && isset($vars['node']->nid)) {
+		if(isset($vars['node']->field_object_id[$lang_code][0]['value'])){
+			$vars['asset_id'] = $vars['node']->field_object_id[$lang_code][0]['value'];
+		}
+	}
+}
+
