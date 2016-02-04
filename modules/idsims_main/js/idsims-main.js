048cb8fafcb4dfb7009f48b3edb095d4565eb4f7
Joe McPherson <j.m.p.mcpherson@gmail.com>
2016-02-04 12:18:10 +0000

    setup structure/layout for Document URL file uploads

--- modules/idsims_main/js/idsims-main.js
+++ modules/idsims_main/js/idsims-main.js
@@ -3,6 +3,7 @@
   function idsimsmod_function(context)  {
     $(document).ready(function()  {
       if($(".page-node-add-ids-document").length){
+	  /* if on Add IDS Document form */
 	  $("#edit-field-publication-date-und-0-value-day").change(function(){
 	      if($(this).val() > 1){
 		  $('#edit-field-publication-date-uncertain-und-day').attr( "checked", false );
@@ -12,6 +13,13 @@
 	      if($(this).val() > 1){
 		  $('#edit-field-publication-date-uncertain-und-month').attr( "checked", false );
 	      }
+	  });
+      }
+      if($(".node-ids_document-form").length){
+	  /* if on Add OR Edit IDS Document form */
+	  $(".field-name-field-upload-document input.form-file").change(function(){
+	      /* If a Document file is uploaded set the Full Text Document URL */
+	      $(this).closest('.horizontal-tabs-panes').find('.field-name-field-document-url input.form-text').val();
 	  });
       }
     });
